b1d2f6329d9e4bf02085e30758f3b3f98be12da5
yuChao <ipuxin@qq.com>
2016-11-07 22:00:00 +0800

    json封装接口

--- testMy.php
+++ testMy.php
@@ -1,7 +1,6 @@
 <?php
 header("Content-Type:text/html;Charset=utf-8");
-require_once './responseMy.php';
-require_once './fileMy.php';
+require_once './response.php';
 
 $_dbConfig = [
     'host' => '127.0.0.1',
@@ -17,39 +16,8 @@ $_dbConfig = [
     'number' => ['a', 'c', 1, 3, 5],
 ];
 
-$file = new File();
+$file = Response::showJSON(200,'success',$_dbConfig);
 
-//写入缓存
-$file->cacheData('file_put_contents', $_dbConfig);
-if ($file) {
-    echo 'is ok';
-} else {
-    echo 'no';
-}
-
-//获取缓存
-$file->cacheData('file_put_contents');
-if ($file) {
-    echo '获取缓存成功 is ok<br>';
-    var_dump($file->cacheData('file_put_contents'));
-    exit;
-} else {
-    echo 'no';
-}
-
-//删除缓存
-$file->cacheData('file_put_contents', null);
-if ($file) {
-    echo '缓存删除 is ok<br>';
-    exit;
-} else {
-    echo 'no';
-}
-
-//Response::show(200,'成功返回',$_dbConfig,'xml');
-//echo '<hr>';
-//Response::jsonEncode(200,'成功返回',$_dbConfig);
-//Response::xmlEncode(200,'成功返回',$_dbConfig);
 
 
 
